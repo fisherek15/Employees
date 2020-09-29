2690f4d89ab0a84420fda32e79841b24dbfe93ce
fisherek15 <fisherek15@gmail.com>
2020-09-29 21:02:44 +0200

    Base code

--- src/best/programming/Main.java
+++ src/best/programming/Main.java
@@ -1,8 +1,25 @@
 package best.programming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee tom = new Employee("Tom Trimmer", 30);
+        Employee lisa = new Employee("Lisa Adele", 21);
+        Employee adrian = new Employee("Adrian Hummer", 40);
+        Employee andrea = new Employee("Andrea Still", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(tom);
+        employees.add(lisa);
+        employees.add(adrian);
+        employees.add(andrea);
+
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
     }
 }
